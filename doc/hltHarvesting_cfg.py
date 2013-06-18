45d8dcc779cd6feb9a0982e95ce38a19d114e8db
Ihar Marfin <sha1-10c58cbea02fbc0cee0ec2975d4fafb35aac1c77@cern.ch>
2013-06-18 10:16:00 +0000

    New documentation added

--- doc/hltHarvesting_cfg.py
+++ doc/hltHarvesting_cfg.py
@@ -14,7 +14,7 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring("")
 )
-
+#process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 process.DQMStore.collateHistograms = False
 
 process.dqmSaver.convention = 'Offline'
@@ -42,11 +42,25 @@ process.MessageLogger.cerr.DQMFileSaver = cms.untracked.PSet(
        limit = cms.untracked.int32(1000000)
        )
 
-process.dqmSaver.workflow = "/CMSSW_3_1_0/RelVal/TrigVal"
+process.dqmSaver.workflow = "/CMSSW_5_2_7/RelVal/TrigVal"
 process.DQMStore.verbose=0
-process.maxEvents.input = 2
+#process.maxEvents.input = 20
 process.source.fileNames = cms.untracked.vstring(
-'file:step2_RAW2DIGI_RECO_VALIDATION.root'
+# /CMSSW_5_2_7/RelVal/TrigVal
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/8817D14D-3906-E211-B128-0018F3D09700.root",
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/8877CB4D-3906-E211-A77F-0018F3D09700.root",
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/9CBD153B-3906-E211-A63E-002618943810.root",
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/A694FA3D-3906-E211-8AD2-0018F3D0962E.root",
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/B0F7403F-3906-E211-A377-001A928116C4.root",
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/CCA64137-3906-E211-A999-0030486791F2.root",
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/E6FAA136-3906-E211-ADC6-002618943831.root",
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/EE03FA3B-3906-E211-8078-0018F3D095EE.root",
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/F01D2884-4206-E211-8EF5-001A92971B7C.root",
+"/store/relval/CMSSW_5_2_7-START52_V10/RelValTTbar/GEN-SIM-DIGI-RAW-HLTDEBUG/v1/00000/F4BCCF33-3906-E211-A9ED-0026189437E8.root"
+
+
+#'file:step2_RAW2DIGI_RECO_VALIDATION.root'
+#'file:MyOutputFile.root'
 )
 
 
@@ -57,7 +71,7 @@ process.load("L1TriggerConfig.L1GtConfigProducers.L1GtConfig_cff")
 #process.load("Configuration.StandardSequences.L1TriggerDefaultMenu_cff")
 #process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.Geometry.GeometryIdeal_cff")
-
+process.load("HLTriggerOffline.Btag.Validation.hltJetMCTools_cff")
 process.validation = cms.Path(
     process.hltvalidation
    # process.HLTMuonVal
@@ -75,11 +89,15 @@ process.post_validation = cms.Path(
     process.hltpostvalidation
     )
 
+process.extra_jetmctools  = cms.Path( process.hltJetMCTools )
+
+
 process.EDMtoMEconv_and_saver= cms.Path(process.EDMtoMEConverter*process.dqmSaver)
 
 process.schedule = cms.Schedule(
+	process.extra_jetmctools,
     process.validation,
-#    process.post_validation,
+    process.post_validation,
     process.EDMtoMEconv_and_saver
     )
 
